a2ebee668452f2c8091c54b3cb2bc848c3f57bdb
Brian McIlwain <technicallyemployed@gmail.com>
2022-11-08 18:50:06 -0600

    Remove create and update stamps

--- prisma/seed/data/authors.ts
+++ prisma/seed/data/authors.ts
@@ -3,32 +3,24 @@ export const authors = [
     id: 'ckzn4u8jv0004clc80l9fejjk',
     firstName: 'Brandon',
     lastName: 'Sanderson',
-    bio: 'Brandon Sanderson is an American author of epic fantasy and science fiction. He is best known for the Cosmere fictional universe, in which most of his fantasy novels, most notably the Mistborn series and The Stormlight Archive, are set.',
-    createdAt: 1644869514954,
-    updatedAt: 1644869514955
+    bio: 'Brandon Sanderson is an American author of epic fantasy and science fiction. He is best known for the Cosmere fictional universe, in which most of his fantasy novels, most notably the Mistborn series and The Stormlight Archive, are set.'
   },
   {
     id: 'ckzn5dg400561clc89jv1k07o',
     firstName: 'Patrick',
     lastName: 'Rothfuss',
-    bio: "Patrick James Rothfuss (born June 6, 1973) is an American writer of epic fantasy. He is best known for his projected trilogy The Kingkiller Chronicle, which has won him several awards, including the 2007 Quill Award for his debut novel, The Name of the Wind. Its sequel, The Wise Man's Fear, topped The New York Times Best Seller list.",
-    createdAt: 1644870411216,
-    updatedAt: 1644870488717
+    bio: "Patrick James Rothfuss (born June 6, 1973) is an American writer of epic fantasy. He is best known for his projected trilogy The Kingkiller Chronicle, which has won him several awards, including the 2007 Quill Award for his debut novel, The Name of the Wind. Its sequel, The Wise Man's Fear, topped The New York Times Best Seller list."
   },
   {
     id: 'ckzn5hvii0636clc8mtcbm2em',
     firstName: 'Scott',
     lastName: 'Lynch',
-    bio: 'Scott Lynch (born April 2, 1978)[1] is an American fantasy author who wrote the Gentleman Bastard series of novels. His first novel, The Lies of Locke Lamora, was purchased by Orion Books in August 2004 and published in June 2006 under the Gollancz imprint in the United Kingdom and under the Bantam imprint in the United States. The next two novels in the series, Red Seas Under Red Skies and The Republic of Thieves, were published in 2007 and 2013, respectively.',
-    createdAt: 1644870617802,
-    updatedAt: 1644870617802
+    bio: 'Scott Lynch (born April 2, 1978)[1] is an American fantasy author who wrote the Gentleman Bastard series of novels. His first novel, The Lies of Locke Lamora, was purchased by Orion Books in August 2004 and published in June 2006 under the Gollancz imprint in the United Kingdom and under the Bantam imprint in the United States. The next two novels in the series, Red Seas Under Red Skies and The Republic of Thieves, were published in 2007 and 2013, respectively.'
   },
   {
     id: 'cl0a0o4fn0045xxc86n988k0l',
     firstName: 'Vladimir',
     lastName: 'Putin',
-    bio: 'Soon to become a former president of Russian Federation and convicted war criminal.',
-    createdAt: 1646253233267,
-    updatedAt: 1664317967082
+    bio: 'Soon to become a former president of Russian Federation and convicted war criminal.'
   }
 ];
--- prisma/seed/data/stores.ts
+++ prisma/seed/data/stores.ts
@@ -2,79 +2,53 @@ export const stores = [
   {
     id: 'ckzn54clw0225clc80ot1ar4x',
     name: 'Osaka, Japan',
-    createdAt: 1644869986772,
-    updatedAt: 1644869996982
   },
   {
     id: 'ckzn54xpf0270clc8z2d9jq70',
     name: 'New Delhi, India',
-    createdAt: 1644870014115,
-    updatedAt: 1644870014115
   },
   {
     id: 'ckzn55c910293clc86l5xeo3k',
     name: 'Kiev, Ukraine',
-    createdAt: 1644870032965,
-    updatedAt: 1657912584531
   },
   {
     id: 'ckzn562s30355clc8lc1yf3nh',
     name: 'Petropavlovsk, Russia',
-    createdAt: 1644870067347,
-    updatedAt: 1651859138707
   },
   {
     id: 'ckzn56m9p0375clc8m4v3xpao',
     name: 'New York, USA',
-    createdAt: 1644870092605,
-    updatedAt: 1644870092605
   },
   {
     id: 'ckzn576550402clc8q4ez5fq4',
     name: 'London, UK',
-    createdAt: 1644870118361,
-    updatedAt: 1644870118361
   },
   {
     id: 'ckzn57gvd0430clc8xuyuwcer',
     name: 'Bucharest, Romania',
-    createdAt: 1644870132265,
-    updatedAt: 1644870132266
   },
   {
     id: 'ckzn583v20454clc889p9w7mv',
     name: 'Paris, France',
-    createdAt: 1644870162062,
-    updatedAt: 1644870162062
   },
   {
     id: 'ckzn59jke0476clc8hvspxacd',
     name: 'Brussels, Belgium',
-    createdAt: 1644870229070,
-    updatedAt: 1644870229070
   },
   {
     id: 'ckzn59uxw0499clc8hmysn1n5',
     name: 'Berlin, Germany',
-    createdAt: 1644870243812,
-    updatedAt: 1656672480234
   },
   {
     id: 'ckzn5a67w0522clc84ds8a6pe',
     name: 'Milano, Italy',
-    createdAt: 1644870258428,
-    updatedAt: 1644870258428
   },
   {
     id: 'ckzn5mwbh0768clc8unwkpgys',
     name: 'Frankfurt, Germany',
-    createdAt: 1644870852124,
-    updatedAt: 1644870852125
   },
   {
     id: 'cl0a0s6270091xxc8hoohdt50',
     name: 'Moscow, Russia',
-    createdAt: 1646253421999,
-    updatedAt: 1651859146764
   }
 ];
