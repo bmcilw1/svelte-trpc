48257bf825f501b2af503af1e2b088c3e4803194
Brian McIlwain <technicallyemployed@gmail.com>
2022-11-08 18:46:39 -0600

    Added seed data files

--- prisma/seed/data/books.ts
+++ prisma/seed/data/books.ts
@@ -1,1 +1,171 @@
-export const books = [];
+export const books = [
+  {
+    id: 'ckzn4vm3m0021clc8fgfqlksj',
+    title: 'Elantris',
+    excerpt:
+      "Elantris is a fantasy novel written by American fantasy and science fiction writer Brandon Sanderson. The book was published on April 21, 2005, by Tor Books and was Sanderson's first published book. The book gained generally positive reviews, and has been translated into multiple languages. Two related works, Hope of Elantris and The Emperor's Soul, have been released, and two additional novels in the series have been announced.",
+    authorId: 'ckzn4u8jv0004clc80l9fejjk',
+    price: 9.98,
+    createdAt: 1644869579170,
+    updatedAt: 1645612334428
+  },
+  {
+    id: 'ckzn4wyhb0037clc8wk2ehxhn',
+    title: 'Mistborn: The Final Empire',
+    excerpt:
+      'Mistborn: The Final Empire, also known simply as Mistborn or The Final Empire, is a fantasy novel written by American author Brandon Sanderson. It was published on July 17, 2006, by Tor Books and is the first novel in the Mistborn trilogy, followed by The Well of Ascension in 2007 and The Hero of Ages in 2008.',
+    authorId: 'ckzn4u8jv0004clc80l9fejjk',
+    price: 12.49,
+    createdAt: 1644869641871,
+    updatedAt: 1645613173612
+  },
+  {
+    id: 'ckzn4xtbj0052clc813us9im4',
+    title: 'Mistborn: The Well of Ascension',
+    excerpt:
+      'Mistborn: The Well of Ascension is a fantasy novel written by American author Brandon Sanderson. It was published on August 21, 2007, by Tor Books and is the second novel in the Mistborn trilogy. It is preceded by The Final Empire in 2006 and followed by The Hero of Ages in 2008.',
+    authorId: 'ckzn4u8jv0004clc80l9fejjk',
+    price: 11.490000000000000213,
+    createdAt: 1644869681839,
+    updatedAt: 1644869681839
+  },
+  {
+    id: 'ckzn4yku70067clc8ci4zmelj',
+    title: 'Mistborn: The Hero of Ages',
+    excerpt:
+      'Mistborn: The Hero of Ages is an epic fantasy novel written by American author Brandon Sanderson. It was published on October 14, 2008, by Tor Books and is the third novel in the Mistborn trilogy.[1] It is preceded by The Well of Ascension in 2007 and followed by The Alloy of Law in the Mistborn: Era 2 series, Wax and Wayne in 2011.',
+    authorId: 'ckzn4u8jv0004clc80l9fejjk',
+    price: 11.98,
+    createdAt: 1644869717503,
+    updatedAt: 1644869717503
+  },
+  {
+    id: 'ckzn50ea40133clc8nkd7h5aj',
+    title: 'Warbreaker',
+    excerpt:
+      'Warbreaker is a fantasy novel written by American author Brandon Sanderson. It was published on June 9, 2009 by Tor Books.',
+    authorId: 'ckzn4u8jv0004clc80l9fejjk',
+    price: 9.25,
+    createdAt: 1644869802316,
+    updatedAt: 1644869802316
+  },
+  {
+    id: 'ckzn51ebc0148clc8l3gf4dao',
+    title: 'The Way of Kings',
+    excerpt:
+      'The Way of Kings is an epic fantasy novel written by American author Brandon Sanderson and the first book in The Stormlight Archive series.[2] The novel was published on August 31, 2010, by Tor Books.[3] The Way of Kings consists of one prelude, one prologue, 75 chapters, an epilogue and 9 interludes.[4] It was followed by Words of Radiance in 2014,[5][6][7] Oathbringer in 2017 and Rhythm of War in 2020. A leatherbound edition was released in 2021.',
+    authorId: 'ckzn4u8jv0004clc80l9fejjk',
+    price: 14.980000000000000426,
+    createdAt: 1644869849016,
+    updatedAt: 1644869849016
+  },
+  {
+    id: 'ckzn5248f0164clc86kg85a0a',
+    title: 'Words of Radiance',
+    excerpt:
+      'Words of Radiance is an epic fantasy novel written by American author Brandon Sanderson and the second book in The Stormlight Archive series.[2] The novel was published on March 4, 2014, by Tor Books.[3] Words of Radiance consists of one prologue, 89 chapters, an epilogue and 14 interludes. It is preceded by The Way of Kings and followed by Oathbringer.',
+    authorId: 'ckzn4u8jv0004clc80l9fejjk',
+    price: 13.990000000000000213,
+    createdAt: 1644869882607,
+    updatedAt: 1644869882607
+  },
+  {
+    id: 'ckzn52x9o0179clc8gs2r6f2c',
+    title: 'Oathbringer',
+    excerpt:
+      'Oathbringer is an epic fantasy novel written by American author Brandon Sanderson and the third book in The Stormlight Archive series. It was published by Tor Books on November 14, 2017. Oathbringer consists of one prologue, 122 chapters, 14 interludes and an epilogue. It is preceded by Words of Radiance and followed by Rhythm of War.',
+    authorId: 'ckzn4u8jv0004clc80l9fejjk',
+    price: 15.990000000000000213,
+    createdAt: 1644869920236,
+    updatedAt: 1660385674152
+  },
+  {
+    id: 'ckzn53qzb0195clc8q1htm0i0',
+    title: 'Rhythm of War',
+    excerpt:
+      'Rhythm of War is an epic fantasy novel written by American author Brandon Sanderson and the fourth book in The Stormlight Archive series. It was published by Tor Books on November 17, 2020.[1] Rhythm of War consists of one prologue, 117 chapters, 12 interludes and an epilogue.[2] It is preceded by Oathbringer. It is currently both the longest Sanderson novel and longest in the series at 455,891 words, though Oathbringer has a higher page count.',
+    authorId: 'ckzn4u8jv0004clc80l9fejjk',
+    price: 19.98,
+    createdAt: 1644869958743,
+    updatedAt: 1644869958743
+  },
+  {
+    id: 'ckzn5fr970603clc8c8rn5lsf',
+    title: 'The Name of the Wind',
+    excerpt:
+      "The Name of the Wind, also referred to as The Kingkiller Chronicle: Day One, is a heroic fantasy novel written by American author Patrick Rothfuss. It is the first book in the ongoing fantasy trilogy The Kingkiller Chronicle, followed by The Wise Man's Fear. It was published on March 27, 2007, by DAW Books.",
+    authorId: 'ckzn5dg400561clc89jv1k07o',
+    price: 7.9800000000000004263,
+    createdAt: 1644870518971,
+    updatedAt: 1644870518971
+  },
+  {
+    id: 'ckzn5gm4m0619clc8ekvizogt',
+    title: "The Wise Man's Fear",
+    excerpt:
+      'n the second day of his recounting the story of his life to Chronicler at the Waystone Inn, Kvothe continues the narrative commenced in The Name of the Wind, wherein a younger Kvothe pursues his education at the University. There, he carries on a feud with fellow student Ambrose, culminating in Ambrose getting him brought up on charges of Consortation with Demonic Powers, a capital crime, for having called the Name of the Wind. Despite successfully defending himself in court, Kvothe has guaranteed himself an extremely high term tuition due to the negative attention he has attracted at the University. Kvothe follows the advice of his friends and teachers at the University and decides to take a term off to chase the wind. To postpone having to pay his debt to the loan shark Devi, he uses several of his more prized possessions as collateral before setting off.',
+    authorId: 'ckzn5dg400561clc89jv1k07o',
+    price: 9.99,
+    createdAt: 1644870558982,
+    updatedAt: 1644870558982
+  },
+  {
+    id: 'ckzn5iua90657clc8s01xxr62',
+    title: 'The Lies of Locke Lamora',
+    excerpt:
+      'The Lies of Locke Lamora is a 2006 fantasy novel by American writer Scott Lynch, the first book of the Gentleman Bastard series. Elite con artists calling themselves the "Gentleman Bastards" rob the rich of the city of Camorr, based on late medieval Venice but on an unnamed world.[2] Two stories interweave: in the present, the Gentleman Bastards fight a mysterious Grey King taking over the criminal underworld; alternate chapters describe the history of Camorr and the Gentleman Bastards, in particular Locke Lamora.',
+    authorId: 'ckzn5hvii0636clc8mtcbm2em',
+    price: 11.98,
+    createdAt: 1644870662865,
+    updatedAt: 1644870662865
+  },
+  {
+    id: 'ckzn5jg9w0672clc8vawolsn2',
+    title: 'Red Seas Under Red Skies',
+    excerpt:
+      'Red Seas Under Red Skies is a fantasy novel by American writer Scott Lynch, the second book (of a projected seven)[1] in the Gentleman Bastard series. It continues the adventures of protagonist Locke Lamora and his friend Jean Tannen as they arrive on the exotic shores of Tal Verrar, where they must face the dangers of their past, as well as new rivals that wish to stop them at all costs while they try to pull their most ambitious con yet.',
+    authorId: 'ckzn5hvii0636clc8mtcbm2em',
+    price: 15.5,
+    createdAt: 1644870691364,
+    updatedAt: 1644870691364
+  },
+  {
+    id: 'ckzn5jyxd0687clc8s3jybv90',
+    title: 'The Republic of Thieves',
+    excerpt:
+      "The Republic of Thieves is a fantasy novel by American writer Scott Lynch, the third book in the Gentleman Bastard series. It details the continuing adventures of Locke Lamora and Jean Tannen, as well as Sabetha, Locke's previously absent love interest. The book was released on October 8, 2013 in the US and October 10 2013 in the UK & Commonwealth. It is published by Random House and Orion.",
+    authorId: 'ckzn5hvii0636clc8mtcbm2em',
+    price: 19.99,
+    createdAt: 1644870715537,
+    updatedAt: 1644870715537
+  },
+  {
+    id: 'ckzzfr5t80208jtc8sd17w65u',
+    title: 'Skyward',
+    excerpt:
+      'Skyward is a 2018 young adult science fiction novel written by American author Brandon Sanderson. It is the first in a series of four books.',
+    authorId: 'ckzn4u8jv0004clc80l9fejjk',
+    price: 12.98,
+    createdAt: 1645613441324,
+    updatedAt: 1645613640131
+  },
+  {
+    id: 'cl0a0rnb70070xxc80ok3vp3r',
+    title: 'How the world became my hostage',
+    excerpt:
+      '“Russia’s borders do not end anywhere.”\n\nIn November 2016, Putin spoke at a televised awards ceremony for geography students and asked a 9-year-old boy where Russia’s borders end. When the student answered, “At the Bering Strait, near the U.S.,” the Russian president laughed and corrected him: “Russia’s borders do not end anywhere.” Soon after Putin added that it was a joke. But according to another Russian saying, “Each joke has only part of the joke.”',
+    authorId: 'cl0a0o4fn0045xxc86n988k0l',
+    price: 0.01,
+    createdAt: 1646253397699,
+    updatedAt: 1657912564399
+  },
+  {
+    id: 'cl15avbu20021c5c8lhq8ib7p',
+    title: 'Bringing Russia back to the Dark Ages',
+    excerpt: 'How I managed to turn my own country back to the Middle Ages.',
+    authorId: 'cl0a0o4fn0045xxc86n988k0l',
+    price: 0.01,
+    createdAt: 1648144817066,
+    updatedAt: 1664317949093
+  }
+];
