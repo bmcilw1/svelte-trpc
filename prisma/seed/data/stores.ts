814931cc82ecdf01439c4df5b752f82718755a90
Brian McIlwain <technicallyemployed@gmail.com>
2022-11-08 18:51:48 -0600

    Ran format

--- prisma/seed/data/stores.ts
+++ prisma/seed/data/stores.ts
@@ -1,54 +1,54 @@
 export const stores = [
   {
     id: 'ckzn54clw0225clc80ot1ar4x',
-    name: 'Osaka, Japan',
+    name: 'Osaka, Japan'
   },
   {
     id: 'ckzn54xpf0270clc8z2d9jq70',
-    name: 'New Delhi, India',
+    name: 'New Delhi, India'
   },
   {
     id: 'ckzn55c910293clc86l5xeo3k',
-    name: 'Kiev, Ukraine',
+    name: 'Kiev, Ukraine'
   },
   {
     id: 'ckzn562s30355clc8lc1yf3nh',
-    name: 'Petropavlovsk, Russia',
+    name: 'Petropavlovsk, Russia'
   },
   {
     id: 'ckzn56m9p0375clc8m4v3xpao',
-    name: 'New York, USA',
+    name: 'New York, USA'
   },
   {
     id: 'ckzn576550402clc8q4ez5fq4',
-    name: 'London, UK',
+    name: 'London, UK'
   },
   {
     id: 'ckzn57gvd0430clc8xuyuwcer',
-    name: 'Bucharest, Romania',
+    name: 'Bucharest, Romania'
   },
   {
     id: 'ckzn583v20454clc889p9w7mv',
-    name: 'Paris, France',
+    name: 'Paris, France'
   },
   {
     id: 'ckzn59jke0476clc8hvspxacd',
-    name: 'Brussels, Belgium',
+    name: 'Brussels, Belgium'
   },
   {
     id: 'ckzn59uxw0499clc8hmysn1n5',
-    name: 'Berlin, Germany',
+    name: 'Berlin, Germany'
   },
   {
     id: 'ckzn5a67w0522clc84ds8a6pe',
-    name: 'Milano, Italy',
+    name: 'Milano, Italy'
   },
   {
     id: 'ckzn5mwbh0768clc8unwkpgys',
-    name: 'Frankfurt, Germany',
+    name: 'Frankfurt, Germany'
   },
   {
     id: 'cl0a0s6270091xxc8hoohdt50',
-    name: 'Moscow, Russia',
+    name: 'Moscow, Russia'
   }
 ];
